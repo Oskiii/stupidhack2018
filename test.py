6955ee5f7ab8cb98579ccd3901947175a338e431
Max Samarin <maxim.samarin@gmail.com>
2018-04-13 19:41:36 +0300

    main loop

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-# hello
+import time
+
+def main():
+    # initialization
+
+    print("\n snakecharmer.io  H A S   B E E N   I N I T I A T E D \n")
+
+
+    while True:
+        time.sleep(0.01)
+
+
+main()
